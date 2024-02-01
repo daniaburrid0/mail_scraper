162ee45a56dc7d460ccbd5a3c05c730dc1c8589d
Daniel Barrientos <danielbarrientos68@gmail.com>
2024-02-01 19:11:03 -0300

    formating

--- sites_to_mail.py
+++ sites_to_mail.py
@@ -7,13 +7,15 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 # Regular expression for finding email addresses
-email_pattern = re.compile(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b')
+email_pattern = re.compile(
+    r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b')
 
 # Setup Selenium with ChromeDriver
 service = Service(ChromeDriverManager().install())
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 driver = webdriver.Chrome(service=service, options=options)
+
 
 def find_emails_on_page(url):
     try:
@@ -25,12 +27,14 @@ def find_emails_on_page(url):
         print(f"Error accessing {url}: {e}")
     return []
 
+
 def find_contact_links(base_url):
     try:
         driver.get(base_url)
         # Find all links on the homepage
         links = driver.find_elements(By.TAG_NAME, "a")
-        contact_links = [link.get_attribute('href') for link in links if 'contact' in link.get_attribute('href').lower()]
+        contact_links = [link.get_attribute(
+            'href') for link in links if 'contact' in link.get_attribute('href').lower()]
         return contact_links
     except TimeoutException:
         print(f"Timeout occurred while accessing {base_url}")
@@ -38,19 +42,24 @@ def find_contact_links(base_url):
         print(f"Error accessing {base_url}: {e}")
     return []
 
+
 # Read the list of websites
 site_list = []  # Replace with your list of sites
+
+
 def load_sites():
     with open('sites.csv', 'r', encoding='utf-8') as file:
         reader = csv.reader(file)
         for row in reader:
             site_list.append(row[0])
+
+
 load_sites()
 
 # Scraping and storing results
 with open('emails_and_urls.csv', 'w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
-    writer.writerow(['Site','URL', 'Email'])
+    writer.writerow(['Site', 'URL', 'Email'])
 
     for site in site_list:
         contact_urls = find_contact_links(site)
